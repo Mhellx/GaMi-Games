dd23a92b70174e0a022541f373867405eb09db41
Michell Pereira <michellpereira78@gmail.com>
2022-06-21 01:45:40 -0300

    update
    Campo minado adicionado

--- games/Campo minado/minesweeper.js
+++ games/Campo minado/minesweeper.js
@@ -14,8 +14,25 @@ window.onload = function(){
     startGame();
 }
 
+// Gerar Minas
+function setMines() {
+    let minesLeft = minesCount;
+    while (minesLeft > 0) {
+        let r = Math.floor(Math.random()* rows);
+        let c = Math.floor(Math.random()* columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if(!minesLocation.includes(id)){
+            minesLocation.push(id);
+            minesLeft -= 1;
+        }
+    }
+}
+
 function startGame() {
     document.getElementById("mines-count").innerText = minesCount;
+    document.getElementById("flag-button").addEventListener("click", setFlag);
+    setMines();
 
     for(let r = 0; r < rows; r++) {
         let row = [];
@@ -23,10 +40,128 @@ function startGame() {
             //<div id="0-0"></div>
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click", clickTile);
             document.getElementById("board").append(tile);
             row.push(tile);
         }
         board.push(row);
     }
     console.log(board);
+}
+
+function setFlag(){
+    if (flagEnabled){
+        flagEnabled = false;
+        document.getElementById("flag-button").style.backgroundColor = "lightgray";
+    }
+    else {
+        flagEnabled = true;
+        document.getElementById("flag-button").style.backgroundColor = "darkgray"
+    }
+}
+
+function clickTile() {
+    if(gameOver || this.classList.contains("tile-clicked")){
+        return;
+    }
+
+    let tile = this;
+    if (flagEnabled) {
+        if (tile.innerText == "") {
+            tile.innerText = "🚩";
+        }
+        else if (tile.innerText == "🚩") {
+            tile.innerText = "";
+        }
+        return;
+    }
+
+    // GAME OVER
+    if (minesLocation.includes(tile.id)) {
+        gameOver = true;
+        revealMines();
+        return;
+    }
+
+    let coords = tile.id.split("-"); // "0-0" -> ["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+    checkMine(r, c);
+}
+
+function revealMines(){
+    for(let r = 0; r < rows; r++){
+        for(let c = 0; c < columns; c++){
+            let tile = board[r][c];
+            if(minesLocation.includes(tile.id)){
+                tile.innerText = "💣";
+                tile.style.backgroundColor = "red";
+            }
+        }
+    }
+}
+
+function checkMine(r, c) {
+    if(r < 0 || r >= rows || c < 0 || c >= columns){
+        return;
+    }
+    if(board[r][c].classList.contains("tile-clicked")){
+        return;
+    }
+
+    board[r][c].classList.add("tile-clicked");
+    tilesClicked += 1;
+
+    let minesFound = 0;
+
+    // checar minas
+
+    // topo
+    minesFound += checkTile(r-1, c-1); // topo esquerdo
+    minesFound += checkTile(r-1, c); // meio
+    minesFound += checkTile(r-1, c+1); // top direito
+
+    // direita e esquerda
+    minesFound += checkTile(r, c+1); // direita
+    minesFound += checkTile(r, c-1); // esquerda
+
+    // inferior
+    minesFound += checkTile(r+1, c-1); // inferior esquerdo
+    minesFound += checkTile(r+1, c); // meio
+    minesFound += checkTile(r+1, c+1); // inferior direito
+
+    if(minesFound > 0) {
+        board[r][c].innerText = minesFound;
+        board[r][c].classList.add("x" + minesFound.toString());
+    }
+    else{
+        // topo
+        checkMine(r-1, c-1); // topo esquerdo
+        checkMine(r-1, c); // topo
+        checkMine(r-1, c+1); // topo direito
+
+        // direita e esquerda
+        checkMine(r, c+1); // direita
+        checkMine(r, c-1); // esquerda
+
+        // inferior
+        checkMine(r+1, c-1); // inferior esquerdo
+        checkMine(r+1, c); // meio
+        checkMine(r+1, c+1); // inferior direito
+    }
+
+    if(tilesClicked == rows * columns - minesCount){
+        document.getElementById("mines-count").innerText = "Limpo!";
+        gameOver = true;
+    }
+}
+
+function checkTile(r, c){
+    if(r < 0 || r >= rows || c < 0 || c >= columns){
+        return 0;
+    }
+    if(minesLocation.includes(r.toString()+ "-" + c.toString())){
+        return 1;
+    }
+    return 0;
 }
